114584675a308fa760a1d86f0717fd1dcc21fee9
L7ONEL <lionelardusso4730@gmail.com>
2024-10-09 17:29:01 -0300

    se arregló un problema con las librerias.

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,21 @@
 import { Component } from "react";
+import { Redirect, Route, Switch } from "wouter";
+import './App.css';
 
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {}
+  }
+
+  render(){
+    return(
+      <>
+        <Switch>
+          
+        </Switch>
+      </>
+    )
+  }
+
+}
