08a598c2b027fd2e3492ae50ed0a456183724e9a
KevinV86 <kv759668@gmail.com>
2024-11-07 17:01:21 -0300

    se realizo la conexion del frontend (login y register) al backend correctamente donde el usuario cuando se registre y se guarden en la API y  al inicio de sesion verfique si el usuario esta registrado lo deja entrar al home con header logeado

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,90 @@
-import { Component } from "react";
-import { Redirect, Route, Switch } from "wouter";
-import Home from "./home/Home";
-import Register from "./register/Register";
-import Login from "./login/Login";
-import Favoritos from "./favoritos/Favoritos";
-import MisPropiedades from "./misPropiedades/MisPropiedades";
-import VerCasa from "./verCasa/VerCasa";
-import Pago from "./pago/Pago";
-import PagoRealizado from "./pagoRealizado/PagoRealizado";
-import './App.css';
-import FormularioEntradaPropiedad from "./publicar-casa/FormularioEntradaPropiedad";
-import Editar from "./editar-usuario/Editar";
-import './App.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import EditarPropiedades from "./editar-propiedades/EditarPropiedades";
+// App.jsx  
+import { Component } from "react";  
+import { Route, Switch } from "wouter";  
+import Home from "./home/Home";  
+import Register from "./register/Register";  
+import Login from "./login/Login";  
+import Favoritos from "./favoritos/Favoritos";  
+import MisPropiedades from "./misPropiedades/MisPropiedades";  
+import VerCasa from "./verCasa/VerCasa";  
+import Pago from "./pago/Pago";  
+import PagoRealizado from "./pagoRealizado/PagoRealizado";  
+import FormularioEntradaPropiedad from "./publicar-casa/FormularioEntradaPropiedad";  
+import Editar from "./editar-usuario/Editar";  
+import EditarPropiedades from "./editar-propiedades/EditarPropiedades";  
+import './App.css';  
+import 'bootstrap-icons/font/bootstrap-icons.css';  
 
-export default class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {}
-  }
+export default class App extends Component {  
+    constructor(props) {  
+        super(props);  
+        this.state = {  
+            isAuthenticated: false,  
+            userId: null,  
+        };  
+    }  
 
-  render(){
-    return(
-      <>
-        <Switch>
-          <Route path="/">
-            <Home />
-          </Route>
+    handleLogin = (userId) => {  
+        this.setState({ isAuthenticated: true, userId });  
+    };  
 
-          <Route path="/crear-cuenta">
-            <Register/>
-          </Route>
+    handleLogout = () => {  
+        this.setState({ isAuthenticated: false, userId: null });  
+        localStorage.removeItem("token"); // Limpiar el token del localStorage  
+    };  
 
-          <Route path="/iniciar-sesion">
-            <Login />
-          </Route>
+    render() {  
+        return (  
+            <>  
+                <Switch>  
+                    <Route path="/" >  
+                        <Home   
+                            isAuthenticated={this.state.isAuthenticated}   
+                            onLogout={this.handleLogout}   
+                        />  
+                    </Route>  
 
-          <Route path="/editar-usuario">
-          <Editar />
-          </Route>
+                    <Route path="/crear-cuenta">  
+                        <Register />  
+                    </Route>  
 
-          <Route path="/publicar-casa">
-            < FormularioEntradaPropiedad/>
-          </Route>
+                    <Route path="/iniciar-sesion">  
+                        <Login onLogin={this.handleLogin} />  
+                    </Route>  
 
-          <Route path="/favoritos">
-            <Favoritos />
-          </Route>
+                    <Route path="/editar-usuario">  
+                        <Editar />  
+                    </Route>  
 
-          <Route path="/mis-propiedades/editar-casa">
-          <EditarPropiedades/>
-          </Route>
+                    <Route path="/publicar-casa">  
+                        <FormularioEntradaPropiedad />  
+                    </Route>  
 
-          <Route path="/mis-propiedades">
-            <MisPropiedades />
-          </Route>
+                    <Route path="/favoritos">  
+                        <Favoritos />  
+                    </Route>  
 
-          <Route path="/ver-casa">
-            <VerCasa />
-          </Route>
-          
-          <Route path="/ver-casa/pago/:id?">
-            {params => <Pago id={params.id}/>}
-          </Route>
-          
-          <Route path="/ver-casa/pago/pago-realizado">
-            <PagoRealizado />
-          </Route>
-        </Switch>
-      </>
-    )
-  }
+                    <Route path="/mis-propiedades/editar-casa">  
+                        <EditarPropiedades />  
+                    </Route>  
 
+                    <Route path="/mis-propiedades">  
+                        <MisPropiedades />  
+                    </Route>  
+
+                    <Route path="/ver-casa/:id">  
+                        {params => <VerCasa id={params.id} />}  
+                    </Route>  
+
+                    <Route path="/ver-casa/pago/:id?">  
+                        {params => <Pago id={params.id} />}  
+                    </Route>  
+                    
+                    <Route path="/ver-casa/pago/pago-realizado">  
+                        <PagoRealizado />  
+                    </Route>  
+                </Switch>  
+            </>  
+        );  
+    }  
 }
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,38 +1,51 @@
-import { Component } from 'react';
-import HeaderConLogin from '../comun/headerConLogin/HeaderConLogin'
-import HeaderSinLogin from '../comun/HeaderSinLogin';
-import Buscador from './buscador/Buscador';
-import VisualizacionDeCasas from "../comun/visualizaciondecasas/VisualizacionDeCasas"
-import Footer from "../comun/Footer"
+// Home.js  
+import { Component } from 'react';  
+import axios from 'axios';  
+import HeaderConLogin from '../comun/headerConLogin/HeaderConLogin';  
+import HeaderSinLogin from '../comun/HeaderSinLogin';  
+import Buscador from './buscador/Buscador';  
+import VisualizacionDeCasas from "../comun/visualizaciondecasas/VisualizacionDeCasas";  
+import Footer from "../comun/Footer";  
 
-export default class Home extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            header: false,
-            casas: [],
-        }
-    }
+export default class Home extends Component {  
+    constructor(props) {  
+        super(props);  
+        this.state = {  
+            casas: [],  
+        };  
+    }  
 
-    render() {
-        return(
-            <>
-                {this.state.header ? (
-                    <HeaderConLogin />
-                ) : (
-                    <HeaderSinLogin />   
-                )}
+    // Este método se ejecuta cuando el componente se monta  
+    componentDidMount() {  
+        axios.get('http://localhost:4001/api/casas')  
+            .then(response => {  
+                this.setState({ casas: response.data });  
+            })  
+            .catch(error => {  
+                console.error('Error al obtener las casas:', error);  
+            });  
+    }  
 
+    render() {  
+        const { isAuthenticated, onLogout } = this.props;  
 
-                <Buscador />
+        return (  
+            <>  
+                {isAuthenticated ? (  
+                    <HeaderConLogin onLogout={onLogout} /> // Pasamos onLogout props  
+                ) : (  
+                    <HeaderSinLogin />  
+                )}  
 
-                <VisualizacionDeCasas
-                    titulo = "Todas las propiedades"
-                    casas = {this.state.casas}
-                />
+                <Buscador />  
 
-                <Footer />
-            </>
-        )
-    }
+                <VisualizacionDeCasas  
+                    titulo="Todas las propiedades"  
+                    casas={this.state.casas}  
+                />  
+
+                <Footer />  
+            </>  
+        );  
+    }  
 }
